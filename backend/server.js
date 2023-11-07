165ab854a4227b464bce4e6dc22c3b0cd052c127
lilbentley <jujuvonernst@gmail.com>
2023-11-07 16:29:44 +0100

    stable server + optimized

--- backend/server.js
+++ backend/server.js
@@ -5,32 +5,31 @@ const input = require("input");
 const { OpenAI } = require('openai'); 
 const express = require('express');
 
+// Configuration from environment variables
 const apiId = parseInt(process.env.API_ID, 10); 
 const apiHash = process.env.API_HASH; 
 const stringSession = new StringSession(process.env.STRING_SESSION);
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
+// Initialize Telegram client
+const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
 
-const client = new TelegramClient(stringSession, apiId, apiHash, {
-  connectionRetries: 5,
-});
-
+// Initialize OpenAI client
 const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
+// Initialize Express application
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
+// Function to evaluate messages with OpenAI
 async function evaluateMessagesWithAI(prompt) {
   try {
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: 'user', content: prompt }],
       model: 'gpt-3.5-turbo',
     });
-
-    
     const messageContent = chatCompletion.choices[0].message.content;
-    
     return messageContent; 
   } catch (error) {
     console.error('Error calling the OpenAI API:', error);
@@ -38,30 +37,36 @@ async function evaluateMessagesWithAI(prompt) {
   }
 }
 
-(async () => {
-  await client.start({
-    phoneNumber: async () => await input.text("Please enter your number: "),
-    password: async () => await input.text("Please enter your password: "),
-    phoneCode: async () => await input.text("Please enter the code you received: "),
-    onError: (err) => console.log(err),
-  });
-  console.log("Telegram client connected.");
-})();
+// Function to start the Telegram client
+async function startTelegramClient() {
+  try {
+    await client.start({
+      phoneNumber: async () => await input.text("Please enter your number: "),
+      password: async () => await input.text("Please enter your password: "),
+      phoneCode: async () => await input.text("Please enter the code you received: "),
+      onError: (err) => console.log(err),
+    });
+    console.log("Telegram client connected.");
+  } catch (error) {
+    console.error('Failed to start Telegram client:', error);
+  }
+}
 
+// Start the Telegram client
+startTelegramClient();
 
+// Endpoint to get the evaluated number from messages
 app.get('/api/count', async (req, res) => {
   try {
     const channel = await client.getEntity('nexta_live');
     const messages = await client.getMessages(channel, { limit: 10 });
     const messagesText = messages.map(m => m.message).join('\n');
-    const prompt = `You have one goal to only spit out a number from 1 to 5. Only use this numerical Output. There will be a number of short messages that describe the situation in the world. Evaluate  the entirety of them and how bad they are  on a scale of 1 (GOOD) to 5(WORST). Take into perspective the whole situation in the world. Dont overreact, use 4-5 only if really bad stuff happens. Here are the messages: :\n${messagesText}`; // Your existing prompt
+    const prompt = `Please provide a numerical assessment of global stability based on the following messages. Assign a score from 1 to 5, where 1 indicates a stable and peaceful global situation (GOOD), and 5 indicates a highly unstable and critical global situation (WORST). Use the score of 4-5 only for situations with severe negative implications or events. Consider the collective impact of these messages on the current state of global affairs. Provide only the numerical score as your response. Here are the messages: ${messagesText} `; // Your existing prompt
 
     const evaluatedNumber = await evaluateMessagesWithAI(prompt);
-    console.log('Fahrenheit Temperature:', evaluatedNumber);
-    res.json({ count: parseInt(evaluatedNumber, 10) }); // Send the number as a JSON response
+    res.json({ count: parseInt(evaluatedNumber, 10) });
   } catch (error) {
-    console.error('An error occurred:', error);
-    res.status(500).send('An error occurred');
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
   }
 });
 
@@ -69,5 +74,3 @@ app.get('/api/count', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
-
-
