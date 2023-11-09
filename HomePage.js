fa4b5e9cf1651480d02b383382bd01344f9c0f0e
lilbentley <jujuvonernst@gmail.com>
2023-11-09 15:11:23 +0100

    Most sever message + message formatting

--- HomePage.js
+++ HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Image, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { View, Image, StyleSheet, TouchableOpacity, Animated, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import { Shadow } from 'react-native-shadow-2';
 
 // Custom hook for rotation animation
 const useRotateAnimation = () => {
@@ -32,21 +33,54 @@ const useRotateAnimation = () => {
   return { rotation, startRotation, stopRotation };
 };
 
+
 const HomePage = () => {
   const [litFiresCount, setLitFiresCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   const { rotation, startRotation, stopRotation } = useRotateAnimation();
+  const [mostSevereArticle, setMostSevereArticle] = useState('');
+
+
+  const formatText = (text) => {
+    if (typeof text !== 'string') {
+      // Return a default message or just an empty string if text is not a string
+      return '';
+    }
+    let formattedText = text
+      .replace(/\d+\)/g, '') // Remove the numbering like "1)"
+      .replace(/([a-z0-9"])([A-Z])/g, '$1. $2') // Add period between sentences without proper punctuation
+      .replace(/\n\s*\n/g, '\n') // Remove empty lines
+      .trimStart(); // Remove spaces at the beginning of the message
+  
+    return formattedText;
+  };
 
   const fetchLitFiresCount = async () => {
     setIsLoading(true);
+    setError(null); // Reset error state before making a new request
     startRotation();
     try {
       const response = await fetch('http://10.0.2.2:3000/api/count');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       const data = await response.json();
-      setLitFiresCount(data.count);
+
+
+      if (data.mostSevereArticleContent) {
+        const formattedText = formatText(data.mostSevereArticleContent);
+        setMostSevereArticle(formattedText);
+      } else {
+        // Handle the case where mostSevereArticleContent is not provided
+        setMostSevereArticle('No content available.');
+      }
+
+
+      
     } catch (error) {
       console.error('Failed to fetch lit fires count:', error);
-      // Consider setting some state here to show an error message to the user
+      setError('Failed to connect to the server. Please try again later.'); // Set error message
     } finally {
       setIsLoading(false);
       stopRotation();
@@ -55,6 +89,7 @@ const HomePage = () => {
 
   useEffect(() => {
     fetchLitFiresCount();
+    setMostSevereArticle('Loading article...');
   }, []);
 
   const fireSpots = Array.from({ length: 5 }, (_, index) => (
@@ -67,10 +102,28 @@ const HomePage = () => {
 
   return (
     <LinearGradient
-      colors={['#c88787', '#7ab0bc']} // Use your own gradient colors
+      colors={['#ad7676', '#5b868f']} // Use your own gradient colors
       style={styles.linearGradient}
     >
       <View style={styles.container}>
+      <Text style={styles.title}>Fahrenheit</Text>
+
+      <View style={styles.articleShadowContainer}>
+          <Shadow
+            style={styles.shadow}
+            startColor={'#00000030'}
+            finalColor={'#00000000'}
+            offset={[0, 0]}
+            distance={10}
+            paintInside={false}
+          >
+            <View style={styles.articleContainer}>
+              <Text style={styles.articleText}>{mostSevereArticle}</Text>
+            </View>
+          </Shadow>
+        </View>
+
+      {error && <Text style={styles.errorText}>{error}</Text>} 
         <View style={styles.fireContainer}>{fireSpots}</View>
         {isLoading ? (
           <Animated.Image
@@ -100,6 +153,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    
   },
   fireContainer: {
     flexDirection: 'row',
@@ -108,12 +162,58 @@ const styles = StyleSheet.create({
     width: 100,
     height: 50,
     resizeMode: 'contain',
-    marginTop: 20,
+    marginTop: 50,
+    
   },
   fireImage: {
     width: 50,
     height: 50,
+    
   },
+  errorText: {
+    color: 'red', // You can style your error message as you like
+    textAlign: 'center',
+    margin: 10,
+  },
+
+  title: {
+    fontSize: 56,
+    fontFamily: 'BonaNova-Regular', // Replace with the actual name of your custom font
+    textAlign: 'center',
+    color: '#efefef',
+    width: 500,
+    height: 200,
+    
+    
+
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: 5, height: 10},
+    textShadowRadius: 70,
+   
+  },
+
+  articleShadowContainer: {
+    alignSelf: 'stretch',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  shadow: {
+    borderRadius: 20,
+    alignSelf: 'stretch', // Ensure the shadow stretches to match the width of the article container
+  },
+  articleContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Semi-transparent white
+    padding: 20,
+    borderRadius: 20,
+    height: 200, // Fixed height for the article container
+  },
+  articleText: {
+    fontFamily: 'BonaNova-Regular', // Replace with the actual name of your custom font
+    fontSize: 18,
+    color: '#333', // Dark text color for readability
+  },
+  
+
 });
 
 export default HomePage;
--- backend/server.js
+++ backend/server.js
@@ -1,111 +1,117 @@
-require('dotenv').config();
-const { TelegramClient } = require("telegram");
-const { StringSession } = require("telegram/sessions");
-const input = require("input");
-const { OpenAI } = require('openai');
-const express = require('express');
+  require('dotenv').config();
+  const { TelegramClient } = require("telegram");
+  const { StringSession } = require("telegram/sessions");
+  const input = require("input");
+  const { OpenAI } = require('openai');
+  const express = require('express');
 
-// Configuration from environment variables
-const apiId = parseInt(process.env.API_ID, 10);
-const apiHash = process.env.API_HASH;
-const stringSession = new StringSession(process.env.STRING_SESSION);
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
-const AIprompt = "As an AI with a conservative stance on global stability ratings, you are to provide STRICTLY only a numerical assessment from 1 to 5 based on the following messages, where 1 represents a completely stable and peaceful global situation, and 5 represents extreme instability and crisis. Ratings of 3 or 4 or  5 are reserved for only the most dire of circumstances, where the events described have profound and widespread negative consequences. Please apply a stringent threshold for these ratings and consider the overall context and historical global stability standards. Provide only the numerical score as your response, and disregard any promotional content. Here are the messages:";
-// Global filter words
-const globalFilterWords = ['globalWord1', 'globalWord2']; // Add global filter words here
+  // Configuration from environment variables
+  const apiId = parseInt(process.env.API_ID, 10);
+  const apiHash = process.env.API_HASH;
+  const stringSession = new StringSession(process.env.STRING_SESSION);
+  const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+  const AIprompt = "Given the following news summaries, provide a numerical escalation level from 0 to 5 based on the overall state of global affairs. The scale is defined as: 0 for no significant events, 1 for minor incidents, 2 for moderate issues, 3 for serious events, 4 for critical incidents, and 5 for catastrophic events such as the start of a new war or worse. The assessment should be conservative, reserving levels 3 to 5 for extremely severe situations. Output the number that corresponds to the escalation level. After the number put the most severe news article behind it like this (Numberof escalation level. Number of most severe News Article). Only output those two Numbers. This is the news articles: ";
+  // Global filter words
+  const globalFilterWords = ['globalWord1', 'globalWord2']; // Add global filter words here
 
-// Channel configurations
-const channels = [
-  {
-    name: 'oldlentach',
-    limit: 5,
-    filterWords: ['#реклама', 'news.lenta.ch'],
-    removeWords: ['removeThisWord'], // Specific filter words for this channel
-  },
+  // Channel configurations
+  const channels = [
+    {
+      name: 'oldlentach',
+      limit: 3,
+      filterWords: ['#реклама', 'news.lenta.ch'],
+      removeWords: ['removeThisWord'], // Specific filter words for this channel
+    },
 
-  {
-    name: 'nexta_live',
-    limit: 5,
-    filterWords: ['#реклама', 'youtu.be'],
-    removeWords: ['@nexta_live'], // Specific filter words for this channel
-  },
-  
-  {
-    name: 'varlamov_news',
-    limit: 5,
-    filterWords: ['#реклама'],
-    removeWords: ['removeThisWord'], // Specific filter words for this channel
-  },
-  // Add more channel configurations here
-];
+    {
+      name: 'nexta_live',
+      limit: 3,
+      filterWords: ['#реклама', 'youtu.be'],
+      removeWords: ['@nexta_live'], // Specific filter words for this channel
+    },
+    
+    {
+      name: 'varlamov_news',
+      limit: 3,
+      filterWords: ['#реклама', 'youtu.be'],
+      removeWords: ['removeThisWord'], // Specific filter words for this channel
+    },
+    // Add more channel configurations here
+  ];
 
-// Initialize Telegram client
-const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
+  // Initialize Telegram client
+  const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
 
-// Initialize OpenAI client
-const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+  // Initialize OpenAI client
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
-// Initialize Express application
-const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.json());
+  // Initialize Express application
+  const app = express();
+  const port = process.env.PORT || 3000;
+  app.use(express.json());
 
-// Function to evaluate messages with OpenAI
-async function evaluateMessagesWithAI(prompt) {
-  try {
+  // Function to evaluate messages with OpenAI
+  async function evaluateMessagesWithAI(prompt) {
+    try {
 
-    // const chatCompletion = await openai.chat.completions.create({
-    //   messages: [{ role: 'user', content: prompt }],
-    //   model: 'gpt-3.5-turbo',
-    //   seed: 1337,
-    // });
+      // const chatCompletion = await openai.chat.completions.create({
+      //   messages: [{ role: 'user', content: prompt }],
+      //   model: 'gpt-3.5-turbo',
+      //   seed: 1337,
+      // });
 
-    const chatCompletion = await openai.completions.create({
-      prompt: prompt,
-      model: 'gpt-3.5-turbo-instruct',
-      seed: 1337,
-    });
+      const chatCompletion = await openai.completions.create({
+        prompt: prompt,
+        model: 'gpt-3.5-turbo-instruct',
+        seed: 1337344,
+        temperature: 0.1,
+        
+      });
 
 
-    const messageContent = chatCompletion.choices[0].text;
-    return messageContent;
-  } catch (error) {
-    console.error('Error calling the OpenAI API:', error);
-    throw error;
+      const messageContent = chatCompletion.choices[0].text;
+      return messageContent;
+    } catch (error) {
+      console.error('Error calling the OpenAI API:', error);
+      throw error;
+    }
   }
-}
 
-// Function to start the Telegram client
-async function startTelegramClient() {
-  try {
-    await client.start({
-      phoneNumber: async () => await input.text("Please enter your number: "),
-      password: async () => await input.text("Please enter your password: "),
-      phoneCode: async () => await input.text("Please enter the code you received: "),
-      onError: (err) => console.log(err),
-    });
-    console.log("Telegram client connected.");
-  } catch (error) {
-    console.error('Failed to start Telegram client:', error);
+  // Function to start the Telegram client
+  async function startTelegramClient() {
+    try {
+      await client.start({
+        phoneNumber: async () => await input.text("Please enter your number: "),
+        password: async () => await input.text("Please enter your password: "),
+        phoneCode: async () => await input.text("Please enter the code you received: "),
+        onError: (err) => console.log(err),
+      });
+      console.log("Telegram client connected.");
+      // console.log(client.session.save());
+    } catch (error) {
+      console.error('Failed to start Telegram client:', error);
+    }
   }
-}
 
-// Start the Telegram client
-startTelegramClient();
-function removeWordsFromMessage(message, removeWords) {
-  let modifiedMessage = message;
-  removeWords.forEach(word => {
-    // Replace the word with an empty string
-    const regex = new RegExp(word, 'gi'); // 'gi' for case-insensitive and global match
-    modifiedMessage = modifiedMessage.replace(regex, '');
-  });
-  return modifiedMessage.trim(); // Trim the message after replacements
-}
-// Function to fetch and filter messages from a channel
+  // Start the Telegram client
+  startTelegramClient();
+  function removeWordsFromMessage(message, removeWords) {
+    let modifiedMessage = message;
+    removeWords.forEach(word => {
+      // Replace the word with an empty string
+      const regex = new RegExp(word, 'gi'); // 'gi' for case-insensitive and global match
+      modifiedMessage = modifiedMessage.replace(regex, '');
+    });
+    return modifiedMessage.trim(); // Trim the message after replacements
+  }
+  // Function to fetch and filter messages from a channel
+  // Function to fetch and filter messages from a channel
 async function fetchAndFilterMessages(channelConfig) {
   const channel = await client.getEntity(channelConfig.name);
   const messages = await client.getMessages(channel, { limit: channelConfig.limit });
   const combinedFilterWords = [...globalFilterWords, ...channelConfig.filterWords];
+
+  let messageIndex = 1; // Start numbering from 1
 
   return messages
     .filter(m => m.message && m.message.trim() !== '')
@@ -113,12 +119,21 @@ async function fetchAndFilterMessages(channelConfig) {
       // Remove specified words from each message
       const messageWithoutRemovedWords = removeWordsFromMessage(m.message, channelConfig.removeWords);
       // Further filter out any messages that contain the combined filter words
-      return combinedFilterWords.some(word => messageWithoutRemovedWords.includes(word)) ? '' : messageWithoutRemovedWords;
+      const filteredMessage = combinedFilterWords.some(word => messageWithoutRemovedWords.includes(word)) ? '' : messageWithoutRemovedWords;
+      // If the message is not empty after filtering, prepend the index and increment it
+      if (filteredMessage !== '') {
+        const numberedMessage = ` ${messageIndex}) ${filteredMessage}`;
+        messageIndex++; // Increment the message index for the next message
+        return numberedMessage;
+      }
+      return '';
     })
     .filter(m => m !== ''); // Filter out any messages that became empty after word removal
 }
 
-// Endpoint to get the evaluated number from messages
+
+  // Endpoint to get the evaluated number from messages
+  // Endpoint to get the evaluated number from messages
 app.get('/api/count', async (req, res) => {
   try {
     let allTextMessages = [];
@@ -141,16 +156,32 @@ app.get('/api/count', async (req, res) => {
     const prompt = `${AIprompt}  ${allTextMessages.join('\n')} `;
 
     // Evaluate messages with AI
-    const evaluatedNumber = await evaluateMessagesWithAI(prompt);
-    res.json({ count: parseInt(evaluatedNumber, 10) });
-    console.log("Fahrenheit Temperature:"+ evaluatedNumber);
+    const evaluationResponse = await evaluateMessagesWithAI(prompt);
+    const [evaluatedNumber, mostSevereArticleIndex] = evaluationResponse.split('.').map(num => parseInt(num.trim(), 10));
+
+    // Find the most severe article using the index
+    const mostSevereArticle = allTextMessages[mostSevereArticleIndex - 1]; // Adjust for zero-based index
+    console.log("AI Response:", evaluationResponse);
+    // Log the results to the console
+    console.log("Evaluated Number:", evaluatedNumber);
+    console.log("Number of Most Severe News Article:", mostSevereArticleIndex);
+    console.log("Content of Most Severe News Article:", mostSevereArticle);
+
+    // Send the response
+    res.json({
+      count: evaluatedNumber,
+      mostSevereArticleIndex: mostSevereArticleIndex,
+      mostSevereArticleContent: mostSevereArticle
+    });
+
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'An error occurred while processing your request.' });
   }
 });
 
-// Start the Express server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+
+  // Start the Express server
+  app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
